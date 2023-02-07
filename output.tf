7887307251870bc89b88ad37db714922d18ca211
Luciano Sampaio <lsampaioweb@gmail.com>
2023-02-07 00:38:27 -0300

    Fixed the formatting.

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "random_target_node" {
-  value = module.random_target_node
+  value       = module.random_target_node
   description = "All the returned values for the random_target_node module."
 }
 
