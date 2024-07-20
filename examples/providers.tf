fe670d7bcda50a5514a093cefcb3b4b7dcbf198f
Luciano Sampaio <lsampaioweb@gmail.com>
2024-07-20 11:07:10 -0300

    refactor: Change homelab domain from *.homelab to *.lan.homelab.

--- examples/providers.tf
+++ examples/providers.tf
@@ -12,6 +12,6 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url      = "https://kvm.homelab:8006/api2/json"
+  pm_api_url      = "https://kvm.lan.homelab:8006/api2/json"
   pm_api_token_id = "terraform@pve!terraform"
 }
