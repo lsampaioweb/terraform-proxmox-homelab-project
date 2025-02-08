cc0455e33cedff6e9bcdccaa30c943948cc5c1df
Luciano Sampaio <lsampaioweb@gmail.com>
2025-02-08 16:11:44 -0300

    chore: Update provider versions in providers.tf to 3.6.3 and 3.0.1-rc6

--- examples/providers.tf
+++ examples/providers.tf
@@ -3,12 +3,12 @@ terraform {
     random = {
       # https://registry.terraform.io/providers/hashicorp/random/latest
       source  = "hashicorp/random"
-      version = "3.6.2"
+      version = "3.6.3"
     }
     proxmox = {
       # https://registry.terraform.io/providers/Telmate/proxmox/latest
       source  = "Telmate/proxmox"
-      version = "3.0.1-rc3"
+      version = "3.0.1-rc6"
     }
   }
 }
